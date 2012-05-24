43de3143b61ab9c9766392cb9809126063df1e78
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-24 15:37:51 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject recursion "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]
+                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
---
+++ src/looping_is_recursion.clj
@@ -0,0 +1,27 @@
+(ns looping-is-recursion)
+
+(defn power [base exp]
+  ":(")
+
+(defn last-element [a-seq]
+  ":(")
+
+(defn seq= [seq1 seq2]
+  ":(")
+
+(defn find-first-index [pred a-seq]
+  ":(")
+
+(defn avg [a-seq]
+  ":(")
+
+(defn parity [a-seq]
+  ":(")
+
+(defn fast-fibo [n]
+  ":(")
+
+(defn cut-at-repetition [a-seq]
+  ":(")
+
+; %_____%
