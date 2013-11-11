49b09cfefbb230d4447317906801f207a74dcf98
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:37:41 +0200

    Tests as a separate project

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject looping-is-recursion "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje-grader "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/looping-is-recursion "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
